2e5f2713b9e1f7931c34cbe2ecf2fd96e16a71e7
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-02-08 18:28:28 +0300

    fix type-errors

--- scripts/index-titles.ts
+++ scripts/index-titles.ts
@@ -116,22 +116,30 @@ for (const batch of batches) {
   const bulkResponse = await client.bulk({ refresh: true, operations });
 
   if (bulkResponse.errors) {
-    const erroredDocuments = [];
+    const erroredDocuments: {
+      // If the status is 429 it means that you can retry the document,
+      // otherwise it's very likely a mapping error, and you should
+      // fix the document before to try it again.
+      status: any;
+      error: any;
+      operation: any;
+      document: any;
+    }[] = [];
 
     // The items array has the same order of the dataset we just indexed.
     // The presence of the `error` key indicates that the operation
     // that we did for the document has failed.
     bulkResponse.items.forEach((action, i) => {
-      const operation = Object.keys(action)[0]!;
+      const operation = Object.keys(action)[0]! as keyof typeof action;
       if (!action[operation]) return;
 
-      if (action[operation].error) {
+      if (action[operation]!.error) {
         erroredDocuments.push({
           // If the status is 429 it means that you can retry the document,
           // otherwise it's very likely a mapping error, and you should
           // fix the document before to try it again.
-          status: action[operation].status,
-          error: action[operation].error,
+          status: action[operation]!.status,
+          error: action[operation]!.error,
           operation: operations[i * 2],
           document: operations[i * 2 + 1],
         });
